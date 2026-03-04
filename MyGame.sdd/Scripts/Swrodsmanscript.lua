4cf3c8aa9666fc51a0814898f538bb8b3cf01c1a
IsaJoeFeat <Littletexan06@icloud.com>
2026-03-03 23:38:50 -0600

    Adding swordsmanscript file
    didnt know what to change lol lets hope for the best

---
+++ MyGame.sdd/Scripts/Swrodsmanscript.lua
@@ -0,0 +1,94 @@
+-- Body and gun
+local Base = piece "Base"
+local Body = piece "Body"
+local Turret = piece "Turret"
+local TurretMuzzle = piece "TurretMuzzle"
+local Flare = piece "Flare"
+-- Wheels
+local Wheel1 = piece "Wheel1"
+local Wheel2 = piece "Wheel2"
+local Wheel3 = piece "Wheel3"
+local Wheel4 = piece "Wheel4"
+local Wheel5 = piece "Wheel5"
+local Wheel6 = piece "Wheel6"
+local wheel_speed = math.rad(270)
+aimSpeed = 4.0
+--effects
+--local huntermuzzleflash = SFX.CEG
+
+function script.Create()
+	
+end
+
+local function RestoreAfterDelay()
+Sleep(2000)
+Turn(Turret, y_axis, 0, aimSpeed)
+Turn(TurretMuzzle, x_axis, 0, aimSpeed)
+    WaitForTurn(Turret, y_axis)
+	WaitForTurn(TurretMuzzle, x_axis)
+end
+
+
+SIG_DELAYEDSTOP = 1
+SIG_AIM = 2
+function script.StartMoving()
+    Signal(SIG_DELAYEDSTOP)
+    Spin(Wheel1, x_axis, wheel_speed)
+    Spin(Wheel2, x_axis, wheel_speed)
+    Spin(Wheel3, x_axis, wheel_speed)
+    Spin(Wheel4, x_axis, wheel_speed)
+	Spin(Wheel5, x_axis, wheel_speed)
+	Spin(Wheel6, x_axis, wheel_speed)
+end
+
+function script.StopMoving()
+    StartThread(delayedStopAnimation)
+end
+
+
+function delayedStopAnimation()
+    Signal(SIG_DELAYEDSTOP)
+    SetSignalMask(SIG_DELAYEDSTOP)
+    Sleep(250)
+    StopSpin (Wheel1,x_axis)
+    StopSpin (Wheel2,x_axis)
+    StopSpin (Wheel3,x_axis)
+    StopSpin (Wheel4,x_axis)
+	StopSpin (Wheel5,x_axis)
+	StopSpin (Wheel6,x_axis)
+end
+
+----aimining & fire weapon
+function script.AimFromWeapon1() 
+	return Turret
+	
+end
+
+function script.QueryWeapon1() 
+	return Flare 
+end
+
+function script.AimWeapon1( heading, pitch )
+	Signal(SIG_AIM)
+    SetSignalMask(SIG_AIM)
+    Turn(Turret, y_axis, heading, aimSpeed)
+    Turn(TurretMuzzle, x_axis, -pitch, aimSpeed)
+    WaitForTurn(Turret, y_axis)
+	StartThread(RestoreAfterDelay)
+    return true
+end
+
+function script.FireWeapon1()
+		--EmitSfx(Flare, huntermuzzleflash)
+end
+
+---death animation
+function script.Killed(recentDamage, maxHealth, corpsetype)
+	Explode (Body, SFX.SHATTER)
+	local severity = recentDamage / maxHealth
+	if severity <= 0.33 then
+	return 1
+	else
+	return 2 
+	end 
+end
